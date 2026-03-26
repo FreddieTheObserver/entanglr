3a032f922392d324853fb017a9db45d316c36add
layaungphyo <layaungphyo079@gmail.com>
2026-03-26 14:27:53 +0700

    feat/custom error class

---
+++ server/src/lib/apiError.ts
@@ -0,0 +1,35 @@
+
+export class ApiError extends Error {
+  constructor(
+    public statusCode: number,
+    message: string,
+    public isOperational = true,
+  ) {
+    super(message);
+    Object.setPrototypeOf(this, ApiError.prototype);
+  }
+
+  static badRequest(message = 'Bad request') {
+    return new ApiError(400, message);
+  }
+
+  static unauthorized(message = 'Unauthorized') {
+    return new ApiError(401, message);
+  }
+
+  static forbidden(message = 'Forbidden') {
+    return new ApiError(403, message);
+  }
+
+  static notFound(message = 'Not Found') {
+    return new ApiError(404, message);
+  }
+
+  static conflict(message = 'Conflict') {
+    return new ApiError(409, message);
+  }
+
+  static internal(message = 'Internal server error') {
+    return new ApiError(500, message, false);
+  }
+}
